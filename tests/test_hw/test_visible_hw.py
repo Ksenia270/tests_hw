5aad47d3012491ef53201e989f248c92c1241b5f
Ксения <shutovaksenia27@mail.ru>
2025-03-30 21:09:14 +0300

    second

--- tests/test_hw/test_visible_hw.py
+++ tests/test_hw/test_visible_hw.py
@@ -5,7 +5,7 @@ def test_visible_accordion(browser):
     accordion = Accordion(browser)
     accordion.visit()
     assert  accordion.text.visible()
-    assert accordion.heading.click()
+    accordion.heading.click()
     time.sleep(2)
 def test_visible_accordion_default(browser):
     accordion = Accordion(browser)
